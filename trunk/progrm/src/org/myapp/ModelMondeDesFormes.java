467355335e11b80776634f59beea29ef72c9d36c
monchrys <monchrys@ac6a94d4-e245-11dd-9208-8f743628cd7a>
2009-01-20 23:26:51 +0000

    debut de l elaboration du model de formes

---
+++ trunk/progrm/src/org/myapp/ModelMondeDesFormes.java
@@ -0,0 +1,33 @@
+package org.myapp;
+
+import org.asma.cercle;
+
+/**
+ * 
+ * @author christophe
+ *
+ */
+public class ModelMondeDesFormes {
+
+	Lecteur lecteur;
+	/**
+	 * creation en dur des formes
+	 * toutes liées au lecteur.
+	 */
+	
+	public ModelMondeDesFormes(Lecteur lecteur) {
+
+		this.lecteur = lecteur;
+	}
+	
+	public void inittest(){
+
+		for(int i=0;i<10;i++){
+			// on cree les forme en leur
+			ModelForme c = new ModelForme(lecteur);
+
+		}
+
+	}
+	
+}
