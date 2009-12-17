c5b44fba10000a74afcc463d7ae0ff43406969e9
monchrys <monchrys@ac6a94d4-e245-11dd-9208-8f743628cd7a>
2009-12-17 16:37:18 +0000

    Passage a netbean

--- trunk/progrm/src/drawing/GUIHelper.java
+++ trunk/progrm/src/drawing/GUIHelper.java
@@ -1,6 +1,7 @@
 package drawing;
 
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.DisplayMode;
 import java.awt.Frame;
 import java.awt.GraphicsDevice;
@@ -10,66 +11,73 @@ import java.awt.event.WindowEvent;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
+import org.Verbose;
+
 /**
  * @author duj
  *
- * 
+ *
  */
 public class GUIHelper {
-	
-	
-	
-	 private static DisplayMode[] BEST_DISPLAY_MODES = new DisplayMode[] {
+
+	private static DisplayMode[] BEST_DISPLAY_MODES = new DisplayMode[] {
 		 new DisplayMode(1280, 960, 32, 0),
 		 new DisplayMode(1024, 768, 32, 0),
 		 new DisplayMode(640, 480, 32, 0),
 		 new DisplayMode(640, 480, 16, 0),
 		 new DisplayMode(640, 480, 8, 0)
 	    };
-	 
-	 
-	 
+
+
+
 	public static void showOnFrame(JComponent component, String frameName) {
 		JFrame frame = new JFrame(frameName);
 		GraphicsDevice device = null;
-		if(GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices().length == 2){ //si on a deux �ran
+		Verbose.print(	"nous allons allons configurer les écrans",
+						"nombre d'écrans "+ GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices().length,
+						"");
+				// si on a plusieur ecrant
+		if(GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices().length == 2){
 			frame.setLocation(GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[1].getConfigurations()[1].getBounds().x,GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[1].getConfigurations()[1].getBounds().y);
 			device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[1];
-		}
-		else 
+			frame.setUndecorated(true);
+		}		// si on a un seul écran
+		else
 		{
 			frame.setLocation(GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0].getConfigurations()[0].getBounds().x,GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0].getConfigurations()[0].getBounds().y);
 			device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
+			frame.setUndecorated(false);
 		}
-		frame.setUndecorated(true);
-		frame.setIgnoreRepaint(true);
-		
-		
+
+		//frame.setIgnoreRepaint(true);
+
+
 		device.setFullScreenWindow(frame);
 		if (device.isDisplayChangeSupported()) {
             chooseBestDisplayMode(device);
         }
-        
+
 		WindowAdapter wa = new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		};
-		
+
 		frame.addWindowListener(wa);
 		frame.getContentPane().add(component);
+		//frame.setPreferredSize(new Dimension(800, 600));
 		frame.pack();
-		
-		//frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+
+		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
 		frame.setVisible(true);
-		
+
 		//
 		frame.setDefaultLookAndFeelDecorated(true);
-		
-		
+
+
 
 	}
-	
+
 	public static void showOnFrame(Component component, String frameName) {
 		Frame frame = new Frame(frameName);
 		WindowAdapter wa = new WindowAdapter() {
@@ -86,17 +94,17 @@ public class GUIHelper {
 //		frame.setResizable(false);
 
 	}
-	
-	
-	
+
+
+
 	 public static void chooseBestDisplayMode(GraphicsDevice device) {
 	        DisplayMode best = getBestDisplayMode(device);
 	        if (best != null) {
 	            device.setDisplayMode(best);
 	        }
 	    }
-	 
-	 
+
+
 	 private static DisplayMode getBestDisplayMode(GraphicsDevice device) {
 	        for (int x = 0; x < BEST_DISPLAY_MODES.length; x++) {
 	            DisplayMode[] modes = device.getDisplayModes();
@@ -111,7 +119,7 @@ public class GUIHelper {
 	        }
 	        return null;
 	    }
-	 
-	 
+
+
 
 }
---
+++ trunk/progrm/src/org/Verbose.java
@@ -0,0 +1,33 @@
+package org;
+
+import java.util.HashMap;
+
+public class Verbose {
+
+	private static final int MIN_ARGUMENT = 0;
+	private static final int MAX_ARGUMENT = 6;
+
+	private static int level = 1;
+	public HashMap<String, String> option;
+
+	public Verbose(String[] args){
+		option = new HashMap<String, String>();
+		if ((args.length > MAX_ARGUMENT) || (args.length < MIN_ARGUMENT)) {
+			// boucle iterateur tant quela longueur de la chaine superieure a zero{
+
+				// test argument.
+			//}
+		}
+	}
+
+	public static void print(String a,String b,String c){
+		switch(level){
+		case 1:
+			System.out.println(a);
+		case 2:
+			System.out.println(b);
+		case 3:
+			System.out.println(c);
+		}
+	}
+}
