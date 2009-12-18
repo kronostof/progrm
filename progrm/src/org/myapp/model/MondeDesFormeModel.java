73f73bf303208b749e1c956e0df9de27fc8d5663
monchrys <monchrys@ac6a94d4-e245-11dd-9208-8f743628cd7a>
2009-12-18 11:35:15 +0000

    refactoring de la classe ShapeFactory. Les type de forme disponible font maintenant partie d'une enumeration de la factory.

--- progrm/src/org/myapp/model/MondeDesFormeModel.java
+++ progrm/src/org/myapp/model/MondeDesFormeModel.java
@@ -5,37 +5,30 @@ import org.myapp.controle.MondeDesFormeController;
 import org.myapp.controle.MondeDesFormeControllerListener;
 import org.myapp.factory.ShapeFactory;
 
-
 public class MondeDesFormeModel {
 
-//	private EventListenerList listeners;
-//	private MondeDesFormeControllerListener Controleur;
+    public MondeDesFormeModel() {
+        // Lecteur est un thread Singloton. Cette instruction est nécéssaire.
+        new Lecteur();
 
-	
-	//private ShapeFactory shapeFactory;
-	public MondeDesFormeModel() {
-		new Lecteur();
-		new ShapeFactory();
-	}
+    }
 
-/**
- * Construction de l'�tat du monde de d�part
- * @param Controleur
- */
-	public void build(MondeDesFormeController Controleur){
-		ShapeFactory.newShape("Gaze0");
-		 for (int i=0;i<5;i++){
-			 ShapeFactory.newShape("fuite0");
-			 ShapeFactory.newShape("approche1");
-			 ShapeFactory.newShape("approche0");
-			 ShapeFactory.newShape("type1");
-			 ShapeFactory.newShape("type2");
-		}
-	}
+    /**
+     * Construction de l'état du monde de départ
+     * @param Controleur
+     */
+    public void build(MondeDesFormeController Controleur) {
+        ShapeFactory.createShape(ShapeFactory.ShapeType.Gaze0);
+        for (int i = 0; i < 2; i++) {
+            ShapeFactory.createShape(ShapeFactory.ShapeType.fuite0);
+            ShapeFactory.createShape(ShapeFactory.ShapeType.approche1);
+            ShapeFactory.createShape(ShapeFactory.ShapeType.Gaze0.approche0);
+            ShapeFactory.createShape(ShapeFactory.ShapeType.Gaze0.type1);
+            ShapeFactory.createShape(ShapeFactory.ShapeType.Gaze0.type2);
+        }
+    }
 
-	public void addControlleurListner(MondeDesFormeControllerListener Controleur) {
+    public void addControlleurListner(MondeDesFormeControllerListener Controleur) {
 //		this.Controleur = Controleur;
-		
-	}
-	
+    }
 }
