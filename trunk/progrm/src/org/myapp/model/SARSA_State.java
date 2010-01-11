2fa5c85588a7e1ad92cfff44980414138378c230
bonnier.vincent <bonnier.vincent@ac6a94d4-e245-11dd-9208-8f743628cd7a>
2010-01-11 16:33:37 +0000

    Ajout des classes issus du package MDP de remi Coulom après selection, pour associé à chaque forme une propriété d'apprentissage (pour que chaque forme puisse implémenter SARSA).

---
+++ trunk/progrm/src/org/myapp/model/SARSA_State.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.myapp.model;
+
+/**
+ *
+ * @author ter Vincent Bonnier
+ */
+class SARSA_State {
+
+    void getInitState() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
--- trunk/progrm/src/org/myapp/model/Superviseur.java
+++ trunk/progrm/src/org/myapp/model/Superviseur.java
@@ -3,17 +3,6 @@ package org.myapp.model;
 import org.myapp.module.module;
 
 public class Superviseur {
-
-	
-	
-	
-	
-	
-	
-	
-	/**
-	 * ajouter un module en bout de chaine
-	 */
 	public static void addModule(module<?, ?> m,Shape mf){
 		
 	}
