90a65c89f1756f1934641b9a81887a46c63f0205
monchrys <monchrys@gmail.com>
2010-01-25 12:47:41 +0000

    Mise en place des methode liee à  l'utilisation de Sarsa. Creation du paquage Sarsa. La génération des état est posible, la génération des action est en cours.

---
+++ progrm/src/org/myapp/Sarsa/Sarsa_Action.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.myapp.Sarsa;
+
+import java.lang.reflect.Field;
+
+/**
+ *
+ * @author christophe Moncy p0304320
+ */
+public class Sarsa_Action {
+
+    /**
+     * champ sur lequelle l'action agit.
+     */
+Field champs = null;
+int id = -1;
+int state_1;
+int state_2;
+
+}
