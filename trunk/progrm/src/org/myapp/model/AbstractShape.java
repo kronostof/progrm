b90f85a7d37726ec9f557b9a34fa53a37c8da335
bonnier.vincent <bonnier.vincent@ac6a94d4-e245-11dd-9208-8f743628cd7a>
2010-01-26 13:39:16 +0000

    Ajout de notion d'héritage entre abstractShpae, NewShape et Shape désormais NewShape et Shape hérite de la classe abstraite AbstractShape qui n'est plus une interface.

--- trunk/progrm/src/org/myapp/model/AbstractShape.java
+++ trunk/progrm/src/org/myapp/model/AbstractShape.java
@@ -1,14 +1,58 @@
 package org.myapp.model;
+
+import javax.swing.event.EventListenerList;
+
+import org.FormeListener;
+import org.myapp.Lecteur;
+import org.myapp.event.Position;
+import org.myapp.factory.ShapeFactory.ShapeType;
+import org.myapp.flux.FluxPosition;
+import org.myapp.module.manager.ModuleManager;
+
+import com.espertech.esper.client.EPServiceProvider;
+import drawing.shape.VueForme.ShapeForme;
+import java.awt.Color;
+import java.awt.Point;
 /*
  *
  *
  *
  */
-public interface AbstractShape {
+abstract class AbstractShape extends Thread implements FormeListener {
 
-	public static final int CURSOR = 1;
-	public static final int SQUARE = 2;
-	public static final int CIRCLE = 3;
-	public static final int TRANGLE = 4;
+    EPServiceProvider epService;
+    private EventListenerList listeners = new EventListenerList();
+    private String nom;
 
+    /**
+    * Identidiant de la forme associé a l'instance
+    */
+    private int forme;
+    private Position position;	// la position dans le monde.
+    public Color color;
+    public FluxPosition Gaze = new FluxPosition();
+    public ModuleManager poolModule;
+    private ShapeType shapeType;
+    private ShapeForme forme2;
+
+    public static final int CURSOR = 1;
+    public static final int SQUARE = 2;
+    public static final int CIRCLE = 3;
+    public static final int TRANGLE = 4;
+
+    abstract Position getPosition();
+    abstract void addFormeListener(FormeListener listener);
+    abstract FormeListener[] getFormeListeners();
+    abstract void firePositionChangee();
+    abstract void fireCouleurChangee();
+    /*abstract void positionChangee(FormeListener Fl);*/
+    abstract String getNom();
+    abstract FluxPosition getGaze();
+    abstract Color getColor();
+    /*abstract Point getPoint();*/
+    abstract void setForme(int forme);
+    abstract void setType(ShapeType shapeType);
+    abstract int getForme();
+    abstract ShapeType getType();
+    abstract void eloigne(Position data, int paramPas);
 }
